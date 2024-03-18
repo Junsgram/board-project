50a84a3f40de8acb4de83a9dc3e8d4a33ddd5f56
Jungram <yuns951213@gmail.com>
2024-03-19 01:43:13 +0900

    #2 Spring Data REST + HAL Explorer 디펜던시 추가 API 간편하게 작성하기위해 Spring Boot에 기능 추가

--- src/main/java/org/board/boardproject/repository/ArticleRepository.java
+++ src/main/java/org/board/boardproject/repository/ArticleRepository.java
@@ -2,6 +2,9 @@ package org.board.boardproject.repository;
 
 import org.board.boardproject.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// spring Data REST 사용하는 어노테이션 yml파일에 annotation으로 지정하였기 때문에 어노테이션으로 지정해야한다
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
