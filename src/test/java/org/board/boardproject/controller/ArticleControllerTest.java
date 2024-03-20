315eca559f47ef7cb1d0da5065363f10f404c7d0
Jungram <yuns951213@gmail.com>
2024-03-21 03:35:32 +0900

    #14 - 뷰의 이름이 무엇인지 확인하는 메소드 추가 진행 및 상세페이지에 댓글도 함께 되기에 model 추가 진행

--- src/test/java/org/board/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/org/board/boardproject/controller/ArticleControllerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@Disabled("구현 중 ")
+
 @DisplayName(" View 컨트롤러 - 게시글 ")
 // webMvcTest는 지정하지 않으면 모든 컨트롤러를 바라보기에 해당 클래스를 지정해서 사용
 @WebMvcTest(ArticleControllerTest.class)
@@ -24,6 +24,7 @@ class ArticleControllerTest {
         this.mvc = mockMvc;
     }
 
+    @Disabled("구현 중 ")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출 확인 ")
     @Test
     // test mehtod
@@ -33,10 +34,12 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 // content의 내용의 타입을 확인 - view라서 text_html로 진행
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 // view단에서 model의 키의 값들이 있는 지 확인
                 .andExpect(model().attributeExists("aritcles"));
     }
 
+    @Disabled("구현 중 ")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출 확인 ")
     @Test
     // test mehtod
@@ -46,9 +49,12 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 // content의 내용의 타입을 확인 - view라서 text_html로 진행
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
                 // view단에서 model의 키의 값들이 있는 지 확인
-                .andExpect(model().attributeExists("aritcle"));
+                .andExpect(model().attributeExists("aritcle"))
+                .andExpect(model().attributeExists("aritcleComments"));
     }
+    @Disabled("구현 중 ")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출 확인 ")
     @Test
     // test mehtod
@@ -57,9 +63,11 @@ class ArticleControllerTest {
                 // 200 ok가 진행되었는 지 확인하는 메소드 stauts()
                 .andExpect(status().isOk())
                 // content의 내용의 타입을 확인 - view라서 text_html로 진행
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));
     }
 
+    @Disabled("구현 중 ")
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출 확인 ")
     @Test
     // test mehtod
@@ -68,6 +76,7 @@ class ArticleControllerTest {
                 // 200 ok가 진행되었는 지 확인하는 메소드 stauts()
                 .andExpect(status().isOk())
                 // content의 내용의 타입을 확인 - view라서 text_html로 진행
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/hastag"));
     }
 }
