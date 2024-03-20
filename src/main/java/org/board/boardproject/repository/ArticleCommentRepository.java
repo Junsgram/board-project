8c7104b01a14e69cd0c9fb03478e97e28a93f9d4
Jungram <yuns951213@gmail.com>
2024-03-21 02:55:21 +0900

    #2- Querydsl 지원을 이용하여 API 검색 옵션 추가  검색기능과, 를 활용하여 게시글 및 댓글 검색 기능 구현

--- src/main/java/org/board/boardproject/repository/ArticleCommentRepository.java
+++ src/main/java/org/board/boardproject/repository/ArticleCommentRepository.java
@@ -1,9 +1,29 @@
 package org.board.boardproject.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.board.boardproject.domain.ArticleComment;
+import org.board.boardproject.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        // ArticleComments안에 있는 모든 필드에 대한 기본 검색기능을 추가
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
