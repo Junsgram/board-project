a11a7b4945846d4c1b37c57e10fe638d0b43eeb4
Jungram <yuns951213@gmail.com>
2024-03-22 02:44:37 +0900

    #14 - 게시판 페이지에 decoupled logic 적용 타임리프 문법을 html 마크업 페이지에서 분리하고 xml 파일을 사용하여 thymeleaf 활용 진행 테스트 작동은 현재 model의 article값이 null로 들어가있어 추후 값을 적용하여 정상작동 확인 예정

--- src/test/java/org/board/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/org/board/boardproject/controller/ArticleControllerTest.java
@@ -38,7 +38,7 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중 ")
+
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출 확인 ")
     @Test
     // test mehtod
